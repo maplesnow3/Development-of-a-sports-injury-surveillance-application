a4e9c3018b529a4a8b4720c37048fe2bd212c26d
wpynm091 <102715236+wpynm091@users.noreply.github.com>
2022-05-22 12:11:59 +1000

    Update index.tsx

--- front/src/pages/newfrom/index.tsx
+++ front/src/pages/newfrom/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.scss'
 import InjureForm from './Component/injureForm.js'
-import ConcussionForm from './Component/concussionForm.js'
-import { Collapse } from 'antd-mobile'
+
 
 const Newform = ()=>{
   const navigator = useNavigate()
@@ -12,16 +11,7 @@ const Newform = ()=>{
       
       <div className='new_form'>
         <div className='top_bar'>New Form</div>
-          <Collapse defaultActiveKey={['1']}>
-          <Collapse.Panel key='1' title='Injure Form'>
           <InjureForm />
-          </Collapse.Panel>
-          <Collapse.Panel key='2' title='Concussion Form(Optional)'>
-          <ConcussionForm />
-          </Collapse.Panel>
-          
-        </Collapse> 
-
     </div>
   )
 }
