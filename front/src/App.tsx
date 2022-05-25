3ff95bf5f0ab1174845594a21696ab2c72792b20
pzvol <z12321a@hotmail.com>
2022-05-25 13:45:33 +1000

    Added redirect for root

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import React from 'react';
-import {Routes,Route, useNavigate, useLocation } from 'react-router-dom';
+import {Navigate, Routes,Route, useNavigate, useLocation } from 'react-router-dom';
 import routers from './router'
 import './App.css';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate replace to="/home" />} />
       {routers.map(
         (item:{path:'string',element:any})=>{
           return<Route key={item.path} path={item.path}  element={<item.element />} />
